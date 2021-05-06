5ebb74fa8cb42063bf7f60e0a871742774246ff8
sdshaoda <sdshaoda@126.com>
2021-05-06 18:56:58 +0800

    multi layer XOR

---
+++ xor/script.js
@@ -0,0 +1,57 @@
+import * as tf from '@tensorflow/tfjs'
+import * as tfvis from '@tensorflow/tfjs-vis'
+import { getData } from './data'
+
+window.predict = () => {
+  console.log('请等待训练完成后预测')
+}
+
+window.onload = async () => {
+  const data = getData(400)
+
+  tfvis.render.scatterplot(
+    { name: 'XOR 训练集' },
+    {
+      values: [
+        data.filter(p => p.label === 1),
+        data.filter(p => p.label === 0),
+      ],
+    }
+  )
+
+  const model = tf.sequential()
+  model.add(
+    tf.layers.dense({
+      units: 4,
+      inputShape: [2],
+      activation: 'relu',
+    })
+  )
+  model.add(
+    tf.layers.dense({
+      units: 1,
+      activation: 'sigmoid',
+    })
+  )
+  model.compile({
+    loss: tf.losses.logLoss,
+    optimizer: tf.train.adam(0.1),
+  })
+
+  const inputs = tf.tensor(data.map(p => [p.x, p.y]))
+  const labels = tf.tensor(data.map(p => p.label))
+
+  await model.fit(inputs, labels, {
+    epochs: 10,
+    callbacks: tfvis.show.fitCallbacks({ name: '训练过程' }, ['loss']),
+  })
+
+  console.log('训练完成')
+
+  window.predict = async form => {
+    const pred = await model.predict(
+      tf.tensor([[Number(form.x.value), Number(form.y.value)]])
+    )
+    console.log(pred.dataSync()[0])
+  }
+}
